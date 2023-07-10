9194d17bf25db5ff1b6950cfdc100d700727544f
Devin King <DvnKiOS@gmail.com>
2023-07-10 15:59:28 -0400

    Initial Commit

---
+++ ConversionBuddy/ConversionBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionBuddyApp.swift
+//  ConversionBuddy
+//
+//  Created by Devin King on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ConversionBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
